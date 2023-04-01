5b3bbfb6371221d71d6c6bfacd28975c544ed150
mochi-moshi <josephhmarin21@gmail.com>
2023-04-01 06:39:20 -0500

    Fixed assembly size warnings

--- src/common_h/asm.h
+++ src/common_h/asm.h
@@ -9,19 +9,19 @@ static inline void hlt() { __asm__ __volatile__ ("hlt"); }
 __attribute__((always_inline))
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
-    __asm__ __volatile__ ("inb %0, %1" : "=a"(ret) : "Nd"(port));
+    __asm__ __volatile__ ("inb %b0, %1" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 __attribute__((always_inline))
 static inline uint16_t inw(uint16_t port) {
     uint16_t ret;
-    __asm__ __volatile__ ("inw %0, %1" : "=a"(ret) : "Nd"(port));
+    __asm__ __volatile__ ("inw %w0, %1" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 __attribute__((always_inline))
 static inline uint32_t inl(uint16_t port) {
     uint32_t ret;
-    __asm__ __volatile__ ("ind %0, %1" : "=a"(ret) : "Nd"(port));
+    __asm__ __volatile__ ("ind %k0, %1" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 __attribute__((always_inline))
@@ -38,19 +38,22 @@ static inline void insl(uint16_t port, uint32_t *dst, size_t count) {
 }
 __attribute__((always_inline))
 static inline void io_wait() {
-    __asm__ __volatile__ ("outb %0, %1" : : "Nd"(0x80), "al"(0));
+    __asm__ __volatile__ ("outb %0, %b1" : : "Nd"(0x80), "a"(0));
+    __asm__ __volatile__ ("outb %0, %b1" : : "Nd"(0x80), "a"(0));
+    __asm__ __volatile__ ("outb %0, %b1" : : "Nd"(0x80), "a"(0));
+    __asm__ __volatile__ ("outb %0, %b1" : : "Nd"(0x80), "a"(0));
 }
 __attribute__((always_inline))
 static inline void nop() { __asm__ __volatile__ ("nop"); }
 __attribute__((always_inline))
 static inline void outb(uint16_t port, uint8_t value)
-    { __asm__ __volatile__ ("outb %0, %1" : : "Nd"(port), "a"(value)); }
+    { __asm__ __volatile__ ("outb %0, %b1" : : "Nd"(port), "a"(value)); }
 __attribute__((always_inline))
 static inline void outw(uint16_t port, uint16_t value)
-    { __asm__ __volatile__ ("outw %0, %1" : : "Nd"(port), "a"(value)); }
+    { __asm__ __volatile__ ("outw %0, %w1" : : "Nd"(port), "a"(value)); }
 __attribute__((always_inline))
 static inline void outl(uint16_t port, uint32_t value)
-    { __asm__ __volatile__ ("outd %0, %1" : : "Nd"(port), "a"(value)); }
+    { __asm__ __volatile__ ("outd %0, %k1" : : "Nd"(port), "a"(value)); }
 __attribute__((always_inline))
 static inline void outsb(uint16_t port, uint8_t *src, size_t count) {
     __asm__ __volatile__ ("cld; rep; outsb" : "+S"(src), "+c"(count) : "Nd"(port));
